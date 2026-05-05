6dce2d166772173c75ec46f88ed98dea3a37ce63
elifbeyzakucuk <elifbeyza.kucuk@bahcesehir.edu.tr>
2026-05-05 12:38:25 +0300

    Update MainProgram.cpp

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -155,6 +155,7 @@ IntArray::IntArray(const IntArray& other) {
     data = new int[capacity];
     for (int i = 0; i < count; i++)
         data[i] = other.data[i];
+         Tracker::objectCreated();
 }
 
 // Copy Assignment Operator
